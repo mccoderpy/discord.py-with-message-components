202ebfd5bc270738c64daf0d0744e127f5ff5f05
LukeZNotFound <luke.hent3005@gmail.com>
2024-06-09 22:31:59 +0200

    fixed documentation on EntitlementType
    and removed type_checking from .SKU

--- discord/monetization.py
+++ discord/monetization.py
@@ -78,14 +78,6 @@ class SKU:
             This can be used to differentiate between a user and a server subscription.
     """
     __slots__ = ('id', 'type', 'application_id', 'name', 'slug', 'flags',)
-    
-    if TYPE_CHECKING:
-        id: int
-        type: SKUType
-        application_id: int
-        name: str
-        slug: str
-        flags: SKUFlags
 
     def __init__(self, data: m.SKU) -> None:
         self.id: int = int(data['id'])
@@ -145,6 +137,7 @@ class Entitlement:
 
     @property
     def type(self) -> EntitlementType:
+        """:class:`discord.EntitlementType`: The typeof the entitlement."""
         return try_enum(EntitlementType, self._type)
 
     @property
